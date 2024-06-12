dff5dcbe6bc26844a61fa397f271895dee905408
anizzzzz <105500242+anizzzzz@users.noreply.github.com>
2024-06-12 16:02:51 +0000

    membuat halaman search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,23 @@
 class HomeController < ApplicationController
-  def index
-    @articles = Article.all
-  end
-
-  def read
-    @article = Article.where(id: params[:article_id]).first
-  end
-
-  def save_comment
-    @article = Article.where(id: params[:article_id]).first
-    comment = @article.comment.new
-    comment.comment = params[:comment]
-    if comment.save
-      redirect_to read_path(article_id: @article.id, notice: 'Success')
-    else
-      redirect_to read_path(article_id: @article.id, notice: 'Error')
+    def index
+      @articles = Article.all
     end
-  end
+  
+    def read
+      @article = Article.where(id: params[:article_id]).first
+    end
+  
+    def save_comment
+      @article = Article.where(id: params[:article_id]).first
+      comment = @article.comments.new
+      comment.comment = params[:comment]
+      if comment.save
+        redirect_to read_path(article_id: @article.id, notice: 'Success')
+      else
+        redirect_to read_path(article_id: @article.id, notice: 'Error')
+      end
+    end
+  
+    def about
+    end
 end
