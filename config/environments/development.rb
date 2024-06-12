a33b05b90e601f9dbe97a23e030f7867ed1cba1e
anizzzzz <105500242+anizzzzz@users.noreply.github.com>
2024-06-12 05:19:54 +0000

    menghapus controller dan migration yang ada

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "musical-umbrella-q764w7rj529j7r-3000.app.github.dev"
+  config.hosts << "glowing-space-invention-9gpjj7rgxjjf7jjx-3000.app.github.dev"
 end
